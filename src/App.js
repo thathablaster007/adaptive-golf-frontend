057b6001acda8f528b49c3e233a582322ae8816c
Neerav <neerav.aiyappa@gmail.com>
2026-03-13 23:41:21 +0530

    Adaptive Golf

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import logo from './logo1.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="nav-bar">
+          <div className="nav-left">
+            <a href="#about">About</a>
+            <a href="#adaptive-golf">What is Adaptive Golf?</a>
+            <a href="#try-golf">Try Golf</a>
+            <a href="#competition">Competition</a>
+            <a href="#media">Media</a>
+          </div>
+          <div className="nav-center">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="foundation-text">Adaptive Golf Alliance Foundation</div>
+          </div>
+          <div className="nav-right">
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="insta-logo">&#x1F4F7;</a>
+            <a href="#blog">Blog</a>
+            <a href="#get-involved" className="get-involved">Get Involved</a>
+            <a href="#news">News</a>
+          </div>
+        </div>
       </header>
     </div>
   );
