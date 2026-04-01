83b643c73712bfb670cedf4e704bf09d569b033f
Neerav <neerav.aiyappa@gmail.com>
2026-04-01 16:39:10 +0530

    changes v12

--- src/index.js
+++ src/index.js
@@ -3,25 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const optimizeCriticalImages = () => {
-  const maxCriticalImages = 6;
-  const candidates = Array.from(document.images)
-    .filter((img) => img.getBoundingClientRect().top < window.innerHeight * 1.1)
-    .slice(0, maxCriticalImages);
-
-  candidates.forEach((img) => {
-    if (!img.hasAttribute('decoding')) {
-      img.decoding = 'async';
-    }
-    if (!img.hasAttribute('loading')) {
-      img.loading = 'eager';
-    }
-    if (!img.hasAttribute('fetchpriority')) {
-      img.fetchPriority = 'high';
-    }
-  });
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -36,7 +17,4 @@ const applyDesktopZoom = () => {
 
 applyDesktopZoom();
 window.addEventListener('resize', applyDesktopZoom);
-
-const runInIdle = window.requestIdleCallback || ((cb) => window.setTimeout(cb, 800));
-runInIdle(optimizeCriticalImages);
 
